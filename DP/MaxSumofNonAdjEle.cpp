d88c619bb0c40ff2ccf2a891bc79c8332fdadf9a
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2023-01-08 11:10:43 +0530

    Update MaxSumofNonAdjEle.cpp

--- DP/MaxSumofNonAdjEle.cpp
+++ DP/MaxSumofNonAdjEle.cpp
@@ -49,4 +49,26 @@ int maximumNonAdjacentSum(vector<int> &nums){
 ----------
   //tabulation
   
-  
+  //Space Optimization
+    #include <bits/stdc++.h> 
+
+
+//creating a function
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n = nums.size();
+   int prev = nums[0];
+    int prev2 = 0;
+    
+    for(int i=1 ; i < n ; i++) {
+        //take andd not take
+        int take = nums[i];
+        if(n>1) take += prev2; 
+        int notTake = 0+ prev;
+        
+        int curi = max(take , notTake);
+        //poiting to other
+        prev2 = prev;
+        prev = curi;
+    }
+    return prev;
+}
