c10fb95c5866fa6e6bca6d2354cebd34aacaf23a
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2023-01-14 19:28:24 +0530

    Update uniquePathsII.cpp

--- DP/uniquePathsII.cpp
+++ DP/uniquePathsII.cpp
@@ -42,4 +42,44 @@ int mazeObstacles(int n, int m, vector< vector< int> > &mat) {
 -------------
  Tabulation
  
+ class Solution {
+    // public:
+    // int f(int i , int j , vector<vector<int>> &a ,vector<vector<int>> &dp) {
+    //     //three base cases
+    //     if(i >= 0 && j >= 0 && a[i][j] == 1 ) return 0;
+    //     if(i == 0 && j == 0) return 1;
+    //     if(i < 0 || j < 0 ) return 0;
+    //     //adding some condition which satisfies 
+    //     if(dp[i][j] != -1 ) return dp[i][j];
+    //     int up = f(i-1 , j , a , dp);
+    //     int left = f(i,j-1 , a , dp);
+
+    //     return dp[i][j] =  up + left;
+    // }
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        // //memoization
+        // vector<vector<int>> dp(m , vector<int> (n,-1));
+        // return f(m-1 , n-1 , obstacleGrid , dp);
+         int dp[n][m];
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < m ; j++) {
+            if(obstacleGrid[i][j] == 1 ) dp[i][j] =  0;
+            else if ( i == 0 && j == 0 ) dp[i][j] = 1;
+            else {
+               int up = 0 ;
+                int left = 0;
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                    
+                 dp[i][j] = up + left;
+            }
+        }
+    }
+    return dp[n-1][m-1];
+    }
+};
+ 
  
