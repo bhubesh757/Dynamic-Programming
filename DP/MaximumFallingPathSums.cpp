f0217c7fe64cd55c8011b45a40dfcb79b993dd23
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2023-02-16 16:26:44 +0530

    Update MaximumFallingPathSums.cpp

--- DP/MaximumFallingPathSums.cpp
+++ DP/MaximumFallingPathSums.cpp
@@ -65,4 +65,64 @@ int getMaxPathSum(vector<vector<int>> &matrix)
     }
     
 
+// Tabulation method 
+class Solution {
+
+//     int f(int i , int j , vector<vector<int>> & matrix) {
+//       if( i <  0 || j>= matrix[0].size()) return -1e8;
+//         if(i == 0 ) return matrix[0][j];
+
+// //checking the matrixes
+
+// int mid = matrix[i][j] + f(i-1 , j , matrix) ;
+// int leftdiag = matrix[i][j] + f(i-1 , j-1 , matrix) ;
+// int rightdiag = matrix[i][j] + f(i-1 , j+1 , matrix);
+
+// // call the max funciton over it
+
+// return  min(mid , max(leftdiag ,rightdiag ));
+
+// }
+
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        //tabulation
+        vector<vector<int>> dp(n , vector<int>(m , -1));
+
+        for(int j = 0 ; j < m ;j++) {
+            dp[0][j] = matrix[0][j];
+        }
+        for(int i = 1 ; i < n ;i++) {
+                for(int j = 0; j < m ;j++) {
+                    int mid = matrix[i][j] + dp[i-1][j]; 
+                    int leftdiag = matrix[i][j] ;
+                    if(j-1 >=0)
+                    leftdiag += 
+                    dp[i-1][j-1];
+                    else 
+                    leftdiag += 10005; 
+                    int rightdiag = matrix[i][j]; 
+                    if(j+1<m) 
+                    rightdiag += 
+                    dp[i-1][j+1];
+                    else 
+                    rightdiag = 10005;  
+
+                    dp[i][j] = min(mid , min(leftdiag , rightdiag));
+                }
+        }
+
+    
+        int mini = 10005;
+    //calling the funtion throughoutthe top to end
+        for(int j = 0 ; j < m ; j++) {
+            mini = min(mini , dp[n-1][j]);
+        }
+        return mini;
+    }
+};
+
     
