b04fadaf4bffd21859ece6c1fe1400eb58315c01
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2023-02-23 15:15:25 +0530

    Create Number Of Subsets.cpp

---
+++ DP/Number Of Subsets.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h> 
+//create a function
+
+int f(int ind , int sum , vector<int> &num) {
+    if(sum == 0 ) return 1;
+    if(ind == 0 ) return num[0] == sum;
+
+   int notTake = f(ind -1 , sum , num);
+   int take = 0;
+
+   if(num[ind] <= sum) {
+       take = f(ind - 1 , sum - num[ind] , num);
+   }
+   return take + notTake;
+}
+int findWays(vector<int> &num, int tar)
+{
+    int n = num.size();
+    //caling the function
+    return f(n-1 , tar , num);
+}
+
+
+
+// Memoization
+#include <bits/stdc++.h> 
+//create a function
+
+int f(int ind , int sum , vector<int> &num ,vector<vector<int>> &dp) {
+    
+    if(sum == 0 ) return 1;
+    if(ind == 0 ) return num[0] == sum;
+
+    if(dp[ind][sum] != -1) return dp[ind][sum];
+    
+   int notTake = f(ind -1 , sum , num , dp);
+   int take = 0;
+
+   if(num[ind] <= sum) {
+       take = f(ind - 1 , sum - num[ind] , num , dp);
+   }
+   return dp[ind][sum] =  take + notTake;
+}
+int findWays(vector<int> &num, int tar)
+{
+    int n = num.size();
+    vector<vector<int>> dp(n , vector<int>(tar+1 , -1));
+    //caling the function
+    return f(n-1 , tar , num , dp);
+}
+
+//
+tabulation
+
+#include <bits/stdc++.h> 
+//create a function
+
+int findWays(vector<int> &num, int tar)
+{
+    int n = num.size();
+    vector<vector<int>> dp(n , vector<int>(tar+1 , 0));
+    //convert the -1 to 0 for tabulation
+    //base cases
+    for(int i = 0 ; i < n ; i++) dp[i][0] = 1;
+    if(num[0] <= tar) {
+        dp[0][num[0]] = 1; 
+    }
+
+    for(int ind = 1 ; ind < n ; ind++) {
+        for(int sum = 0 ; sum <= tar ; sum++) {
+            //paste the recursion part over here
+            int notTake = dp[ind -1][sum];
+            int take = 0;
+
+   if(num[ind] <= sum) {
+       take = dp[ind - 1][sum- num[ind]];
+   }
+    dp[ind][sum] =  take + notTake;
+        }
+    }
+    return dp[n-1][tar];
+}
+
+
+
