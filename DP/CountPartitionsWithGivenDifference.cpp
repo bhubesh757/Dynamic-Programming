83624d56e24de67efda8f8857baf9969a16eecd7
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2023-03-03 17:42:03 +0530

    Update CountPartitionsWithGivenDifference.cpp

--- DP/CountPartitionsWithGivenDifference.cpp
+++ DP/CountPartitionsWithGivenDifference.cpp
@@ -1,5 +1,4 @@
 Prerequisite is DP 17 
-//Memoization
 #include <bits/stdc++.h> 
 // as pe r the question they mentioned that it should be in 1e9 + 7
 int mod = (int)(1e9 +7);
@@ -33,4 +32,42 @@ int countPartitions(int n, int d, vector<int> &arr) {
 }
 // time limit exceeded time complexity :  O(n*n) 
 
-\
+//Memoization
+#include <bits/stdc++.h> 
+// as pe r the question they mentioned that it should be in 1e9 + 7
+int mod = (int)(1e9 +7);
+int f(int ind , int sum , vector<int> &num ,vector<vector<int>> &dp) {
+    
+    if(ind == 0) {
+        if(sum == 0 && num[0] == 0) return 2;
+        if(sum == 0 || sum == num[0]) return 1;
+        return 0;
+    }
+
+    if(dp[ind][sum] != -1) return dp[ind][sum];
+    
+   int notTake = f(ind -1 , sum , num , dp);
+   int take = 0;
+
+   if(num[ind] <= sum) {
+       take = f(ind - 1 , sum - num[ind] , num , dp);
+   }
+   return dp[ind][sum] =  take + notTake;
+}
+int findWays(vector<int> &num, int tar)
+{
+    int n = num.size();
+    vector<vector<int>> dp(n , vector<int>(tar+1 , -1));
+    //caling the function
+    return f(n-1 , tar , num , dp);
+}
+
+int countPartitions(int n, int d, vector<int> &arr) {
+    int totalSum = 0;
+    for(auto &it : arr) totalSum += it;
+    if( totalSum - d < 0 || (totalSum - d) % 2) return false;
+    return findWays(arr, (totalSum - d) / 2);
+}
+
+
+
